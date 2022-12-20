b37527fa12634d1a3ab7266e0384baa7c101c100
ulugbek erkinov <103308510+ulugbekerkinovich2@users.noreply.github.com>
2022-12-20 14:37:19 +0500

    'sd'

--- uploaddata/settings.py
+++ uploaddata/settings.py
@@ -62,13 +62,17 @@ WSGI_APPLICATION = 'uploaddata.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'postgres3',
+    #     'USER': 'postgres',
+    #     'PASSWORD': '0852',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '5432',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres3',
-        'USER': 'postgres',
-        'PASSWORD': '0852',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
