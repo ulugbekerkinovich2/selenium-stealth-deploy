4d4b7065e2c7af649f7bfc34163235ec7f2cec4e
ulugbek erkinov <103308510+ulugbekerkinovich2@users.noreply.github.com>
2022-12-26 16:39:22 +0500

    'yanaborekan'

--- test.py
+++ test.py
@@ -3,12 +3,12 @@ import speedtest
 from django.db import connection
 
 
-def speedtest1():
-    speed_test = speedtest.Speedtest()
-    upload_speed = speed_test.upload()
-    print("Your Upload speed is", upload_speed / 1024 ** 2)
-    speed = upload_speed / 1024 ** 2
-    return speed
+# def speedtest1():
+#     speed_test = speedtest.Speedtest()
+#     upload_speed = speed_test.upload()
+#     print("Your Upload speed is", upload_speed / 1024 ** 2)
+#     speed = upload_speed / 1024 ** 2
+#     return speed
 
 def wait_t():
     cursor = connection.cursor()
@@ -18,7 +18,7 @@ def wait_t():
     return all_data
 
 
-cursor = connection.cursor()
-cursor.execute("SELECT * FROM upload_data")
-all_data = cursor.fetchall()
-print(all_data)
+# cursor = connection.cursor()
+# cursor.execute("SELECT * FROM upload_data")
+# all_data = cursor.fetchall()
+# print(all_data)
