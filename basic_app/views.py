3c212650e7392b6693237dc282bd419905e9eefe
ulugbek erkinov <103308510+ulugbekerkinovich2@users.noreply.github.com>
2022-12-29 23:29:15 +0500

    'path'

--- basic_app/views.py
+++ basic_app/views.py
@@ -70,7 +70,7 @@ def index(request):
     input_link = "https://intranet.ytit.uz/course/view.php?id=3508"
 
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
