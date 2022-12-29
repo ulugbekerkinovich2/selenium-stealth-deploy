a7d28be8aaba0c43834d5e765644fe1931e2b412
ulugbek erkinov <103308510+ulugbekerkinovich2@users.noreply.github.com>
2022-12-29 22:44:31 +0500

    'addworkspace'

--- test1_automatic.py
+++ test1_automatic.py
@@ -51,12 +51,12 @@ def selenium_upload():
     telebots2('replace_folder_path', replace_folder_path)
     input_link = f"{link_for_get_data}"
 
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+    # chrome_options = webdriver.ChromeOptions()
+    # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    # chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--disable-dev-shm-usage")
+    # chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(executable_path="/workspace/chromedriver.exe")
 
 
     # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
