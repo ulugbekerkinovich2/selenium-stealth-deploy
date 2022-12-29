af48b5e2247b8d4c93915bba31ee37f32b9e976c
ulugbek erkinov <103308510+ulugbekerkinovich2@users.noreply.github.com>
2022-12-29 22:35:48 +0500

    'linux64_108'

--- test1_automatic.py
+++ test1_automatic.py
@@ -52,10 +52,10 @@ def selenium_upload():
     input_link = f"{link_for_get_data}"
 
     chrome_options = webdriver.ChromeOptions()
-    # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    # chrome_options.add_argument("--headless")
-    # chrome_options.add_argument("--disable-dev-shm-usage")
-    # chrome_options.add_argument("--no-sandbox")
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
 
 
