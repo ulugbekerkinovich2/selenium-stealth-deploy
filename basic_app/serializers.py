78d5036e317d906620e275f99825c242cec03567
ulugbek erkinov <103308510+ulugbekerkinovich2@users.noreply.github.com>
2022-12-30 16:35:41 +0500

    'push'

--- basic_app/serializers.py
+++ basic_app/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 
 from basic_app import models
-from test1_automatic import selenium_upload
+# from test1_automatic import selenium_upload
 
 
 class UploadDataSerialzier(serializers.ModelSerializer):
@@ -10,5 +10,5 @@ class UploadDataSerialzier(serializers.ModelSerializer):
         model = models.UploadDatas
 
     def create(self, validated_data):
-        selenium_upload()
+        # selenium_upload()
         return self.validated_data
