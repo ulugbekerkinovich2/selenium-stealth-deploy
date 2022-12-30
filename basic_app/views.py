85ddf8c7b8d150cc215fb1c9fa2f3f04249a3de5
ulugbek erkinov <103308510+ulugbekerkinovich2@users.noreply.github.com>
2022-12-30 11:45:02 +0500

    'zip1'

--- basic_app/views.py
+++ basic_app/views.py
@@ -69,11 +69,11 @@ def up():
 def index(request):
     input_link = "https://intranet.ytit.uz/course/view.php?id=3508"
 
-    # chrome_options = webdriver.ChromeOptions()
-    # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    # chrome_options.add_argument("--headless")
-    # chrome_options.add_argument("--disable-dev-shm-usage")
-    # chrome_options.add_argument("--no-sandbox")
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(executable_path="/usr/local/bin/chromedriver.exe")
 
     # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
