37340c18feeae1c92cd105503a4085adb80b8916
ulugbek erkinov <103308510+ulugbekerkinovich2@users.noreply.github.com>
2022-12-30 12:09:23 +0500

    'zip2'

--- test1_automatic.py
+++ test1_automatic.py
@@ -52,11 +52,11 @@ def selenium_upload():
     input_link = f"{link_for_get_data}"
 
     chrome_options = webdriver.ChromeOptions()
-    # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(executable_path="/usr/local/bin/chromedriver")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
 
 
     # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
