20d0eb3ee02c65be77ee91427f2b093c16e7936c
ulugbek erkinov <103308510+ulugbekerkinovich2@users.noreply.github.com>
2022-12-30 11:40:51 +0500

    'zip1'

--- test1_automatic.py
+++ test1_automatic.py
@@ -51,12 +51,12 @@ def selenium_upload():
     telebots2('replace_folder_path', replace_folder_path)
     input_link = f"{link_for_get_data}"
 
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(executable_path="/usr/local/bin/chromedriver")
+    # chrome_options = webdriver.ChromeOptions()
+    # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    # chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--disable-dev-shm-usage")
+    # chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(executable_path="/usr/local/bin/chromedriver.exe")
 
 
     # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
