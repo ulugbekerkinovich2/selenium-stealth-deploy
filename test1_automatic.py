1672bc27314560d42f94989ab23a6b47af39e8d2
ulugbek erkinov <103308510+ulugbekerkinovich2@users.noreply.github.com>
2023-01-01 14:26:31 +0500

    'exists_browser'

--- test1_automatic.py
+++ test1_automatic.py
@@ -52,9 +52,9 @@ def selenium_upload():
     input_link = f"{link_for_get_data}"
 
     chrome_options = Options()
-    # chrome_options.headless = True
+    chrome_options.headless = True
     chrome_options.add_argument("--no-sandbox")
-    # chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-notifications')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--disable-gpu')
