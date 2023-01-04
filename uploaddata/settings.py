c883bb79167facb4e071c3d629664c037a2f587f
ulugbek erkinov <103308510+ulugbekerkinovich2@users.noreply.github.com>
2023-01-04 16:48:59 +0500

    'normal'

--- uploaddata/settings.py
+++ uploaddata/settings.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -154,3 +156,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
